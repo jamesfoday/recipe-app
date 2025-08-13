cfa415a7119d081b8e56a229656f2059f85a14c8
jamesfoday <jamesfodayofficial@gmail.com>
2025-08-13 06:07:48 +0200

    update

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -13,7 +13,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turaned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
 # Allowed hosts - your Heroku app domain
